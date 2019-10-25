8c4467493b2c26558b3107a025ae2927aaf5d886
Tim Burgess <timburgess@mac.com>
2019-10-25 16:29:19 +1300

    Add contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,97 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 import './Contact.css'
-import Footer from './Footer'
+// import Footer from './Footer'
 
-const Contact = () => (
-  <Fragment>
-    <div className="contact">
-      <ul>
-        <li>email</li>
-        <li>timburgess at mac.com</li>
-        <li>mobile</li>
-        <li>NZ  +64-225-804-656</li>
-        <li>Aust  +61-448-793-972</li>
-      </ul>
-    </div>
-    <Footer />
-  </Fragment>
-)
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 300,
+  },
+  multilineField: {
+    marginTop: 0,
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 300,
+  },
+  button: {
+    margin: theme.spacing(1),
+    width: 300,
+    marginTop: '1rem',
+  },
+}))
+
+const Contact = () => {
+  const classes = useStyles()
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    project: '',
+  })
+  const [showForm, setShowForm] = useState(true)
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value })
+  }
+
+  const handleSubmit = () => {
+    console.log('values', values)
+    setShowForm(false)
+  }
+
+  return (
+    <Fragment>
+      <div className="contact">
+        <form className="contact-form" noValidate autoComplete="off">
+          {showForm ? (
+            <Fragment>
+              <TextField
+                id="name"
+                label="Name"
+                className={classes.textField}
+                value={values.name}
+                onChange={handleChange('name')}
+                margin="normal"
+              />
+              <TextField
+                id="email"
+                label="Email"
+                type="email"
+                className={classes.textField}
+                value={values.email}
+                onChange={handleChange('email')}
+                margin="normal"
+              />
+              <div className="multilineLabel">About Your Project</div>
+              <TextField
+                id="project"
+                multiline
+                rows="6"
+                value={values.project}
+                onChange={handleChange('project')}
+                className={classes.multilineField}
+                margin="normal"
+              />
+              <Button variant="contained" className={classes.button} onClick={handleSubmit}>
+                Submit
+              </Button>
+            </Fragment>
+          ) : (
+            <div className="contact-msg">Thanks for your message. I'll be in touch.</div>
+          )}
+        </form>
+        <div className="contact-details">
+          <div>Contact</div>
+          <div>NZ 022-580-4656</div>
+          <div>Aust 0448-793-972</div>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </Fragment>
+  )
+}
 
 export default Contact
